97b53aa21aef9a96361a01dab99d8bf7a9cc5da3
chrissx <chrissx@chrissx.de>
2020-07-19 17:09:09 +0200

    compliance with what lcc thinks c89 is and fixed what could be considered to be a bug (an unclosed FILE *)

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include "args.h"
 
 #define PROGNAME "sm"
-#define VERSION  "0.0.3"
+#define VERSION  "0.0.4"
 #define YEARS    "2019-2020"
 #define AUTHORS  "Chris Häußler, chrissx Media"
 #define VERSIONINFO PROGNAME" "VERSION"\n(c) "YEARS" "AUTHORS
--- src/run_target.c
+++ src/run_target.c
@@ -21,19 +21,21 @@ void run_target(char *smfile, char *target, char *argv0)
 {
         off_t size;
         struct stat s;
+        char *bfr, *d;
+        int c, m;
+        FILE *f;
         if(stat(smfile, &s))
         {
                 printf("Can't get file size: %s\n", strerror(errno));
                 size = 256 * 1024;
         }
         else size = s.st_size;
-	char bfr[size + strlen(target) + strlen(argv0)];
-	int c;
-	FILE *f = fopen(smfile, "r");
+	bfr = malloc(size + strlen(target) + strlen(argv0));
+	f = fopen(smfile, "r");
 	READ_WHILE(1)
 	{
                 if(SON(c)) continue;
-		char *d = bfr;
+		d = bfr;
                 *d++ = c;
                 /* read the target name */
 		READ_WHILE(!SON(c) && c != '{' && c != '(') *d++ = c;
@@ -58,6 +60,8 @@ void run_target(char *smfile, char *target, char *argv0)
                         {
                                 sprintf(bfr, "sudo %s %s", argv0, target);
                                 SYSTEM(bfr);
+                                fclose(f);
+                                free(bfr);
                                 return;
                         } }
                         OPTIONEND;
@@ -78,13 +82,14 @@ execute:
                 READ_WHILE(c != '}')
                 {
                         if(SON(c)) continue;
-                        int modifier = c;
+                        m = c;
                         d = bfr;
                         READ_WHILE(!NEWLINE(c)) *d++ = c;
                         *d = '\0';
-                        run_command(modifier, bfr);
+                        run_command(m, bfr);
                 }
                 fclose(f);
+                free(bfr);
                 return;
 	}
 }
